283ec60905d5e1ab48de45b4ad154478be870fc1
Jonathan Hsu <johnnhsu1031@gmail.com>
2023-05-31 11:43:36 -0700

    upload

---
+++ src/app.js
@@ -0,0 +1,26 @@
+// const hamburgerButton = document.getElementById('hamburger')
+// const navList = document.getElementById('nav-list')
+
+// function toggleButton() {
+//     navList.classList.toggle('show')
+// }
+
+// hamburgerButton.addEventListener('click', toggleButton)
+
+// window.onload = function() {
+//     var projects = [
+//         { name: 'Project 1', link: 'https://example.com/project1' },
+//         { name: 'Project 2', link: 'https://example.com/project2' },
+//         // Add more projects as needed
+//     ];
+    
+//     var projectList = document.getElementById('project-list');
+//     projects.forEach(function(project) {
+//         var projectDiv = document.createElement('div');
+//         var projectLink = document.createElement('a');
+//         projectLink.textContent = project.name;
+//         projectLink.href = project.link;
+//         projectDiv.appendChild(projectLink);
+//         projectList.appendChild(projectDiv);
+//     });
+// }
